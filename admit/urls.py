cbc5ffc1dee9953f6017c398ed3b8e07dbe514b9
Matidza <Matidza46@gmail.com>
2024-11-24 20:30:39 +0200

    Login Functionality

--- admit/urls.py
+++ admit/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('schools', views.schools, name='schools'),
     path('applications', views.applications, name='applications'),
+     path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- admit/views.py
+++ admit/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import SchoolProfile
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -20,3 +21,32 @@ def schools(request):
 # All Schools page
 def applications(request):
     return render(request, 'applications.html', {})
+
+
+# Loging
+def login_user(request):
+    # Determine if user filled the form
+    if request.method == 'POST':
+        # Inputs linked to to the login.html form 
+        # <input type="text" class="form-control" name="username" placeholder="Username"> 
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            
+            #messages.success(request, ('Login Was Successful!'))
+            return redirect('home')
+        else:
+            #messages.success(request, ('Login Not Successful! Try Again!!'))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+
+# Logout
+def logout_user(request):
+
+    pass
