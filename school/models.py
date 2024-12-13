e018cb8b8e9659d1a835eb187a8891c5d20fe71d
Matidza <Matidza46@gmail.com>
2024-12-13 13:07:05 +0200

    School App, Model.py: Section 21, Curriculum, Umalusi

--- school/models.py
+++ school/models.py
@@ -34,6 +34,11 @@ class SchoolProfile(models.Model):
         ('Public', 'Public School'),
         ('Private', 'Private School'),
     ]
+    UMALUSI = [
+        ('DBE', 'The Department of Basic Education (DBE)'),
+        ('IEB', 'The Independent Examination Board (IEB)'),
+        ('SACAI', 'The South African Comprehensive Assessment Institute (SACAI)')
+    ]
     school = models.CharField(max_length=200, blank=True, choices=(SCHOOL))
     #curriculum
     #grade_levels
@@ -53,58 +58,58 @@ class SchoolProfile(models.Model):
     province = models.CharField(max_length=20, choices=(PROVINCE))
 
     DISTRICT = [
-        ('A', 'Alfred Nzo District'),
-        ('', 'Amajuba District'),
-        ('', 'Amathole District'),
-        ('', 'Bojanala Platinum District'),
-        ('', 'Buffalo City Metropolitan '),
-        ('', 'Cape Winelands District'),
-        ('', 'Capricorn District'),
-        ('', 'Central Karoo District'),
-        ('', 'Chris Hani District '),
-        ('', 'City of Cape Town Metropolitan'),
-        ('', 'City of Johannesburg Metropolitan'),
-        ('', 'City of Tshwane Metropolitan'),
-        ('', 'Dr Kenneth Kaunda District'),
-        ('', 'Dr Ruth Segomotsi Mompati District '),
-        ('', 'Ehlanzeni District '),
-        ('', 'Ekurhuleni Metropolitan '),
-        ('', 'eThekwini Metropolitan '),
-        ('', 'Fezile Dabi District '),
-        ('', 'Frances Baard District '),
-        ('', 'Garden Route District '),
-        ('', 'Gert Sibande District '),
-        ('', 'Harry Gwala District '),
-        ('', 'iLembe District'),
-        ('', 'Joe Gqabi District'),
-        ('', 'John Taolo Gaetsewe District'),
-        ('', 'King Cetshwayo District '),
-        ('', 'Lejweleputswa District '),
-        ('', 'Mangaung Metropolitan '),
-        ('', 'Mopani District'),
-        ('', 'Namakwa District'),
-        ('', 'Nelson Mandela Bay Metropolitan'),
-        ('', 'Ngaka Modiri Molema District '),
-        ('', 'Nkangala District '),
-        ('', 'OR Tambo District '),
-        ('', 'Overberg District'),
-        ('', 'Pixley ka Seme District '),
-        ('', 'Sarah Baartman District '),
-        ('', 'Sedibeng District '),
-        ('', 'Sekhukhune District'),
-        ('', 'Thabo Mofutsanyana District '),
-        ('', 'Ugu District'),
-        ('', 'uMgungundlovu District'),
-        ('', 'uMkhanyakude District '),
-        ('', 'uMzinyathi District '),
-        ('', 'uThukela District'),
-        ('', 'Vhembe District'),
-        ('', 'Waterberg District'),
-        ('', 'West Coast District '),
-        ('', 'West Rand District '),
-        ('', 'Xhariep District '),
-        ('', 'ZF Mgcawu District '),
-        ('', 'Zululand District'),
+        ('Alfred Nzo District', 'Alfred Nzo District'),
+        ('amajuba', 'Amajuba District'),
+        ('amathole', 'Amathole District'),
+        ('bojanala platinum', 'Bojanala Platinum District'),
+        ('buffalo city', 'Buffalo City Metropolitan '),
+        ('capa winelands', 'Cape Winelands District'),
+        ('capricorn', 'Capricorn District'),
+        ('central karoo', 'Central Karoo District'),
+        ('chris hani', 'Chris Hani District '),
+        ('city of cape town', 'City of Cape Town Metropolitan'),
+        ('city of johannesburg', 'City of Johannesburg Metropolitan'),
+        ('city of tshwane', 'City of Tshwane Metropolitan'),
+        ('dr kenneth kaunda', 'Dr Kenneth Kaunda District'),
+        ('dr ruth segomotsi mompati', 'Dr Ruth Segomotsi Mompati District '),
+        ('ehlanzeni', 'Ehlanzeni District '),
+        ('ekurhuleni', 'Ekurhuleni Metropolitan '),
+        ('ethekwini', 'eThekwini Metropolitan '),
+        ('fezile dabi', 'Fezile Dabi District '),
+        ('frances baard', 'Frances Baard District '),
+        ('garden route', 'Garden Route District '),
+        ('gert sibande', 'Gert Sibande District '),
+        ('harry gwala', 'Harry Gwala District '),
+        ('ilembe', 'iLembe District'),
+        ('joe gqabi', 'Joe Gqabi District'),
+        ('john taolo gaetsewe', 'John Taolo Gaetsewe District'),
+        ('king cetshwayo', 'King Cetshwayo District '),
+        ('lejweleputswa', 'Lejweleputswa District '),
+        ('mangaung', 'Mangaung Metropolitan '),
+        ('mopani', 'Mopani District'),
+        ('namakwa', 'Namakwa District'),
+        ('nelson mandela bay', 'Nelson Mandela Bay Metropolitan'),
+        ('ngaka modiri molema', 'Ngaka Modiri Molema District '),
+        ('nkangala', 'Nkangala District '),
+        ('or tambo', 'OR Tambo District '),
+        ('overberg', 'Overberg District'),
+        ('pixley ka seme', 'Pixley ka Seme District '),
+        ('sarah baartman', 'Sarah Baartman District '),
+        ('sedibeng', 'Sedibeng District '),
+        ('sekhukhune', 'Sekhukhune District'),
+        ('thabo mofutsanyana', 'Thabo Mofutsanyana District '),
+        ('ugu', 'Ugu District'),
+        ('umgungundlovu', 'uMgungundlovu District'),
+        ('umkhanyakude', 'uMkhanyakude District '),
+        ('umzinyathi', 'uMzinyathi District '),
+        ('uthukela', 'uThukela District'),
+        ('vhembe', 'Vhembe District'),
+        ('waterberg', 'Waterberg District'),
+        ('west coast', 'West Coast District '),
+        ('west rand', 'West Rand District '),
+        ('xhariep', 'Xhariep District '),
+        ('zf mgcawu', 'ZF Mgcawu District '),
+        ('zululand', 'Zululand District'),
         
 
 
@@ -117,13 +122,25 @@ class SchoolProfile(models.Model):
 
 
     CIRCUIT = [
-        ('Soutpansberg East', 'Soutpansberg East'),
+        ('Soutpansberg East', 'Soutpansberg East Circuit'),
+        ('Kgakotlou ', 'Kgakotlou '),
+        ('Lebowakgomo', 'Lebowakgomo'),
+        ('Mankweng ', 'Mankweng '),
+        ('Pietersburg ', 'Pietersburg '),
+        ('Seshego ', 'Seshego '),
+        ('Bochum East ', 'Bochum East '),
+        ('Potgietersrus', 'Potgietersrus'),
+        
+
+
     ]
     circurt = models.CharField(max_length=200,default="",choices=(CIRCUIT))
 
     CURRICULUM = [
+        ('CAPS', 'CAPS curriculum'),
+        ('Cambridge', 'Cambridge curriculum')
     ]
-    curriculum = models.CharField(max_length=100, blank=True)
+    curriculum = models.CharField(max_length=100, blank=True, choices=(CURRICULUM ))
 
     GRADES = [
     ]
@@ -150,12 +167,21 @@ class SchoolProfile(models.Model):
     number_of_learners = models.CharField(max_length=100, blank=True)
 
     # School Fees and Finance
-    section_21 = models.CharField(max_length=100)
-    school_fees = models.CharField(max_length=100)
-    QUANTILE = [
-
+    SECTION = [
+        ('Section 21 (Self Reliant)', 'Yes'),
+        ('Section 21 (Self Reliant)', 'No'),
     ]
-    quintile_Level = models.CharField(max_length=100)
+    section_21 = models.CharField(max_length=100, choices=(SECTION))
+    school_fees = models.CharField(max_length=100)
+    
+    QUANTILE = [
+        ('Quintile 1', 'Quintile 1'),
+        ('Quintile 2', 'Quintile 2'),
+        ('Quintile 3', 'Quintile 3'),
+        ('Quintile 4', 'Quintile 4'),
+        ('Quintile 5', 'Quintile 5'),
+    ]
+    quintile_Level = models.CharField(max_length=100, choices=(QUANTILE))
 
 
     image = models.ImageField(upload_to='uploads/schoolprofile/', default=datetime.datetime.today)
