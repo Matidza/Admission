a64c925c784e446e93a4bef7594f9c0bc7b97207
Matidza <Matidza@gmail.com>
2024-12-28 00:05:53 +0200

    User profile extended, Portals created

--- admit/admin.py
+++ admit/admin.py
@@ -6,7 +6,18 @@ from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Profile)
-#admin.site.register(AdmissionFormModel)
-#admin.site.register(ApplicationStatus)
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
+# External User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User 
+    field = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re-register the new way
+admin.site.register(User, UserAdmin)
