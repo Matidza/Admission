9e4ba370d8caada9fadefa8cfbb37ebdd548863d
Matidza M.Z <125007667+Matidza@users.noreply.github.com>
2025-04-26 11:32:10 +0200

    Update settings.py

--- Admission/settings.py
+++ Admission/settings.py
@@ -95,10 +95,12 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-
 DATABASES = {
-  'default': dj_database_url.parse(os.environ['VEN'], conn_max_age=600)
+    'default': dj_database_url.parse(
+        os.environ['VEN'],
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
